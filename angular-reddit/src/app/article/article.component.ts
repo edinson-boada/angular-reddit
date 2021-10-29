d86a16319918bbd12090561ab685f933e9df82be
paolo-boada-unmsm <edinson.boada@unmsm.edu.pe>
2021-10-29 14:41:21 -0500

    add article component

---
+++ angular-reddit/src/app/article/article.component.ts
@@ -0,0 +1,26 @@
+import { Component, HostBinding, OnInit, Input } from '@angular/core';
+import { Article } from './article.model';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css']
+})
+export class ArticleComponent implements OnInit {
+  @HostBinding('attr.class') cssClass = 'row';
+  @Input() article: Article;
+  constructor() {
+    // article is populated by the Input now,
+    // so we don't need anything here
+  }
+  voteUp(): boolean {
+    this.article.voteUp();
+    return false;
+  }
+  voteDown(): boolean {
+    this.article.voteDown();
+    return false;
+  }
+  ngOnInit() {
+  }
+}
